468f1b0398e59f640f442ef777a7b25ad8b33215
Mounir Benabderrahmane <bnmounir7@gmail.com>
2020-03-02 14:35:04 -0500

    static build for deployment

---
+++ precache-manifest.4198fd0fed737e3d5d4d9ab058cc3dd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bdfda17918928c69f3e9b1356a1357d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4c4c9eaa649c6894fae",
+    "url": "/static/js/2.e4544c86.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.e4544c86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9955ca556ec58481cde",
+    "url": "/static/js/main.5f2fb854.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
